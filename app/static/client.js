3f23b0fb480c490033d54ccd3497cd7a5531afd7
lucaspedrozaem <56172594+lucaspedrozaem@users.noreply.github.com>
2020-12-02 15:47:32 +0100

    Update client.js

--- app/static/client.js
+++ app/static/client.js
@@ -39,3 +39,92 @@ function analyze() {
   xhr.send(fileData);
 }
 
+function startup() {
+    video = document.getElementById('video');
+    canvas = document.getElementById('canvas');
+    photo = document.getElementById('photo');
+    startbutton = document.getElementById('startbutton');
+
+    // access video stream from webcam
+    navigator.mediaDevices.getUserMedia({
+            video: true,
+            audio: false
+        })
+        // on success, stream it in video tag
+        .then(function(stream) {
+            video.srcObject = stream;
+            video.play();
+        })
+        .catch(function(err) {
+            console.log("An error occurred: " + err);
+        });
+
+    video.addEventListener('canplay', function(ev) {
+        if (!streaming) {
+            height = video.videoHeight / (video.videoWidth / width);
+
+            if (isNaN(height)) {
+                height = width / (4 / 3);
+            }
+
+            video.setAttribute('width', width);
+            video.setAttribute('height', height);
+            canvas.setAttribute('width', width);
+            canvas.setAttribute('height', height);
+            streaming = true;
+        }
+    }, false);
+
+    startbutton.addEventListener('click', function(ev) {
+        takepicture();
+        ev.preventDefault();
+    }, false);
+
+    clearphoto();
+}
+
+function clearphoto() {
+    var context = canvas.getContext('2d');
+    context.fillStyle = "#AAA";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    var data = canvas.toDataURL('image/png');
+    photo.setAttribute('src', data);
+}
+
+
+function takepicture() {
+    var context = canvas.getContext('2d');
+    if (width && height) {
+        canvas.width = width;
+        canvas.height = height;
+        context.drawImage(video, 0, 0, width, height);
+
+        var data = canvas.toDataURL('image/png');
+        photo.setAttribute('src', data);
+        
+        var xhr = new XMLHttpRequest();
+        var loc = window.location;
+        xhr.open("POST", `${loc.protocol}//${loc.hostname}:${loc.port}/analyze`,
+          true);
+        xhr.onerror = function() {
+          alert(xhr.responseText);
+        };
+        xhr.onload = function(e) {
+          if (this.readyState === 4) {
+            var response = JSON.parse(e.target.responseText);
+            el("result-label").innerHTML = `Result = ${response["result"]}`;
+          }
+          el("startbutton").innerHTML = "Take photo";
+        };
+
+        var fileData = new FormData();
+        fileData.append("file", data);
+        xhr.send(fileData);
+      
+      
+    } else {
+        clearphoto();
+    }
+}
+
