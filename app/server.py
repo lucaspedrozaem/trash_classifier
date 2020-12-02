1bdeb953b3e60217ba2f5f720183b03cb248f9e3
lucaspedrozaem <56172594+lucaspedrozaem@users.noreply.github.com>
2020-12-02 13:11:23 +0100

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1tWugBCXnSm0DasLzbVVxq-xtVwccDmIF'
+export_file_url = 'https://www.dropbox.com/s/efx5uzs9ieloimj/trash.pkl?dl=1'
 export_file_name = 'trash.pkl'
 
 classes = ['cardboard', 'glass', 'metal', 'paper', 'plastic', 'trash']
